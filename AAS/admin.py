19367330d6137a36be15f54236049a2b709a1fbe
Us4m <usamathegreat7@icloud.com>
2022-12-14 11:58:20 +0500

    nn

--- AAS/admin.py
+++ AAS/admin.py
@@ -6,7 +6,7 @@ from .models import AttendanceImage
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Account
-
+ 
 from .models import Todo
 
 
